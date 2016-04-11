2afad4584188455c08ac3f252bae349764d933ee
patrick <patrick@patrick-Aspire-5750>
2016-04-11 17:00:52 -0300

    Correção no plugin datetime

--- wd-admin/application/apps/projects/plugins_input/datetime_/Datetime_.php
+++ wd-admin/application/apps/projects/plugins_input/datetime_/Datetime_.php
@@ -2,7 +2,7 @@
 if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-class Datetime{
+class Datetime_{
     public function input($value) {
         if (!empty($value)) {
             return preg_replace('/([0-9]{2})\/([0-9]{2})\/([0-9]{4}) (.*)/', '$3-$2-$1 $4', $value);
