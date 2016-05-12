dad67d9360dfc7b7a36ebc03f7f0449f1bd798e4
patrick <contato@widedevelop.com>
2016-05-12 01:20:58 -0300

    Correções no layout

--- wd-admin/application/views/home/index.php
+++ wd-admin/application/views/home/index.php
@@ -3,8 +3,5 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 ?>
-<!-- page content -->
-<div class="right_col" role="main">
-    <h3>Seja bem vindo, selecione uma opção ao lado.</h3>
-    <div class="clearfix"></div>
-</div>
+<h3>Seja bem vindo, selecione uma opção ao lado.</h3>
+<div class="clearfix"></div>
