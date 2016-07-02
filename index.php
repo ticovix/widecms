6d09a9748206cb183901ac4e0a689a4bcc90481d
Patrick Oliveira <contato@widedevelop.com>
2016-07-02 13:00:24 -0300

    Index inserida para verificar se o cms foi instalado

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+if(is_file('../install.php')){
+    header('Location: ../install.php');
+    die();
+}
+
+echo "<h1>Parece que ocorreu tudo bem com a instalação :] <br> Crie um projeto.</h1>";
