1336f86ac648576d91f47e5a01c69743a5cfa48c
Patrick Oliveira <contato@widedevelop.com>
2016-08-09 01:25:07 -0300

    Otimização no script do app users

--- wd-admin/application/apps/users/assets/js/index.js
+++ wd-admin/application/apps/users/assets/js/index.js
@@ -1,17 +1,11 @@
 $(function(){
     var del = false;
     $(".multiple_delete").change(function(){
-        del = false;
-        $(".multiple_delete").each(function(){
-            if($(this).prop('checked')){
-                del = true;
-                return false;
-            }
-        });
-        if(del){
-            $("#btn-del-all").removeClass('hide');
+        var is_checked = $(".multiple_delete").is(":checked");
+        if(is_checked){
+            $("#btn-del-all").removeClass("hide");
         }else{
-            $("#btn-del-all").addClass('hide');
+            $("#btn-del-all").addClass("hide");
         }
     });
 });
