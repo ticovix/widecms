b9f578948d4412aa0b02496c99f0691386e56718
Patrick Oliveira <contato@widedevelop.com>
2016-08-28 20:37:20 -0300

    Alterações no ckeditor para o funcionamento do novo plugin de upload

--- wd-admin/application/apps/projects/plugins_input/ckeditor/assets/gallery/js/upload.js
+++ wd-admin/application/apps/projects/plugins_input/ckeditor/assets/gallery/js/upload.js
@@ -1,73 +1,36 @@
 $(function () {
-    var field;
-    var modal = $("#gallery");
-    var content_files = $("#files-list");
-    /*
-     * Dropzone
-     */
-    if (typeof myDropzone == "function") {
-        myDropzone.on("complete", function (file) {
-            if ($("#files-content").data('gallery') == "ckeditor") {
-                ck_files_list();
-            }
-        });
-    }
-    /*
-     * Function to list files
-     */
-    function ck_files_list() {
-        var content = $("#files-list");
-        content.html("Carregando..");
-        $.ajax({
-            url: url + "apps/gallery/files-list",
-            dataType: "json",
-            type: "POST",
-            data: {limit: 12},
-            success: function (data) {
-                var template = new EJS({url: url + "application/apps/projects/plugins_input/ckeditor/assets/gallery/ejs/list-files.ejs"}).render({data: data, url: url, app_path: app_path});
-                content.html(template);
-            }
-        });
-    }
-
-    // Insere o botão para inserir imagem
+    // Insere o botão para inserir arquivos no ckeditor
+    var exts = ['jpg', 'jpeg', 'png', 'gif'];
     $(".form-group").each(function () {
         var index = $(".form-group").index(this);
-        var field = $(".label-field").eq(index).data("field");
+        var field = $(".label-field").eq(index).data("field") + "_field";
         if ($(".input-field").eq(index).hasClass("ckeditor")) {
+
             $("#btn-save-change").addClass("hide");
+            var btn_index = $(".content-field .btn-gallery-ckeditor").length;
             $(".content-field").eq(index).prepend(
-                    $("<a>").attr({"href": "javascript:void(0);", "data-field": field})
-                    .attr({"data-toggle": "modal", "data-target": "#gallery"})
+                    $("<a>").attr({"href": "javascript:void(0);"})
+                    .addClass("btn-gallery-ckeditor")
                     .html('<i class="fa fa-file-image-o"></i> Inserir arquivos')
                     .addClass('pull-right btn btn-default btn-sm btn-upload')
                     );
-        }
-    });
-    $("#data-project").on("click", ".btn-upload", function () {
-        var text = $(this).html();
-        field = $(this).data("field") + "_field";
-        if ($("#files-content").data('gallery') != "ckeditor") {
-            var template = new EJS({url: url + "application/apps/projects/plugins_input/ckeditor/assets/gallery/ejs/base-files.ejs"}).render();
-            $("#files-content").data("gallery", "ckeditor").html(template);
-            ck_files_list();
-        }
-    });
+            $(".content-field .btn-gallery-ckeditor:eq(" + btn_index + ")").gallery({
+                limit_select: 1,
+                files_selecteds: [],
+                reset_selecteds: true,
+                complete: function (file) {
+                    var file_split = file.split(".");
+                    var ext = file_split[file_split.length - 1];
+                    if ($.inArray(ext, exts) != '-1') {
+                        var html = '<img src="' + base_url + 'wd-content/upload/' + file + '">';
+                    } else {
+                        var html = '<a href="' + base_url + 'wd-content/upload/' + file + '">Arquivo</a>';
+                    }
+                    CKEDITOR.instances[field].insertHtml(html);
+                }
+            });
 
-    modal.on("click", ".btn-add", function () {
-        if (field != null || field != undefined) {
-            var index = $(".btn-add").index(this);
-            var file = $(".file").eq(index).data('file');
-            var exts = ['jpg', 'jpeg', 'png', 'gif'];
-            var file_split = file.split(".");
-            var ext = file_split[file_split.length - 1];
-            if ($.inArray(ext, exts) != '-1') {
-                var html = '<img src="' + base_url + 'wd-content/upload/' + file + '">';
-            } else {
-                var html = '<a href="' + base_url + 'wd-content/upload/' + file + '">Arquivo</a>';
-            }
-            CKEDITOR.instances[field].insertHtml(html);
+
         }
-        modal.modal('toggle');
     });
 });
