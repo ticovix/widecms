42fcf79871b83da4a2d93279d4d0f4295472e8a5
Patrick Oliveira <contato@widedevelop.com>
2016-11-25 18:39:19 -0200

    Correção de carregamento do xml

--- wd-admin/application/apps/projects/config/permissions.php
+++ wd-admin/application/apps/projects/config/permissions.php
@@ -7,7 +7,7 @@ $CI = &get_instance();
 $CI->load->model('../apps/projects/models/projects_model', null, false, true);
 $CI->load->model('../apps/projects/models/pages_model', null, false, true);
 $CI->load->model('../apps/projects/models/sections_model', null, false, true);
-$CI->load->library('../apps/projects/libraries/config_page');
+$CI->load->library('../apps/projects/libraries/config_xml');
 $projects = $CI->projects_model->list_projects_permissions();
 if ($projects) {
     foreach ($projects as $project) {
@@ -31,17 +31,17 @@ if ($projects) {
                         $config = $path_projects . $dir_project . '/' . $dir_page . '/' . $dir_section . '/config.xml';
                         $module = $path_modules . $dir_project . '/' . $dir_page . '/' . $dir_section . '/controllers/' . ucfirst($dir_section) . '.php';
                         $method = $slug_project . '-' . $slug_page . '-' . $slug_section;
-                        $page = 'project/'.$slug_project . '/' . $slug_page . '/' . $slug_section;
+                        $page = 'project/' . $slug_project . '/' . $slug_page . '/' . $slug_section;
                         if (!is_file($module)) {
                             // Verifica se possui listagem de registros na seção, se existir insere mais duas permissões
                             if (is_file($config)) {
-                                $config = $CI->config_page->load_config($dir_project, $dir_page, $dir_section);
+                                $config = $CI->config_xml->load_config($dir_project, $dir_page, $dir_section);
                                 if (isset($config['list']) && count($config['list']) > 0) {
                                     $permission[$page][$method] = '<strong>' . $name_project . ' - ' . $name_page . ' - ' . $name_section . '</strong>';
                                     $permission[$page][$page . '/' . 'create'][$method . '-create'] = 'Criar';
                                     $permission[$page][$page . '/' . 'edit/.*'][$method . '-edit'] = 'Editar';
                                     $permission[$page][$page . '/' . $slug_section . '/' . 'remove/.*'][$method . '-remove'] = 'Remover';
-                                }else{
+                                } else {
                                     $permission[$page][$method] = '<strong>' . $name_project . ' - ' . $name_page . ' - ' . $name_section . '</strong> - Editar';
                                 }
                             }
