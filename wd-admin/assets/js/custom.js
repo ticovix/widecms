65286648e549d5624f0f51c10eb831427ba24355
Patrick Oliveira <contato@widedevelop.com>
2017-01-31 10:13:19 -0200

    Inserção de layout do dropdown

--- wd-admin/assets/js/custom.js
+++ wd-admin/assets/js/custom.js
@@ -1,16 +1,16 @@
 var URL = window.location.href.split('?')[0],
-    $BODY = $('body'),
-    $MENU_TOGGLE = $('#menu_toggle'),
-    $SIDEBAR_MENU = $('#sidebar-menu'),
-    $SIDEBAR_FOOTER = $('.sidebar-footer'),
-    $LEFT_COL = $('.left_col'),
-    $RIGHT_COL = $('.right_col'),
-    $NAV_MENU = $('.nav_menu'),
-    $FOOTER = $('footer');
-    $LOGO = $('.site_title');
+        $BODY = $('body'),
+        $MENU_TOGGLE = $('#menu_toggle'),
+        $SIDEBAR_MENU = $('#sidebar-menu'),
+        $SIDEBAR_FOOTER = $('.sidebar-footer'),
+        $LEFT_COL = $('.left_col'),
+        $RIGHT_COL = $('.right_col'),
+        $NAV_MENU = $('.nav_menu'),
+        $FOOTER = $('footer');
+$LOGO = $('.site_title');
 
 // Sidebar
-$(function() {
+$(function () {
     // Send csrf by ajax
     $.ajaxSetup({
         data: {
@@ -23,8 +23,8 @@ $(function() {
         $RIGHT_COL.css('min-height', $(window).height());
 
         var bodyHeight = $BODY.height(),
-            leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
-            contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
+                leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+                contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
         // normalize content
         contentHeight -= $NAV_MENU.height() + $FOOTER.height();
@@ -34,7 +34,7 @@ $(function() {
     setContentHeight();
 
     // toggle small or large menu
-    $MENU_TOGGLE.on('click', function() {
+    $MENU_TOGGLE.on('click', function () {
         if ($BODY.hasClass('nav-md')) {
             $BODY.removeClass('nav-md').addClass('nav-sm');
             $LEFT_COL.removeClass('scroll-view').removeAttr('style');
@@ -43,7 +43,7 @@ $(function() {
                 $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
             }
 
-            $LOGO.children('img').attr('src',url+'assets/images/widecms_xs.png');
+            $LOGO.children('img').attr('src', url + 'assets/images/widecms_xs.png');
         } else {
             $BODY.removeClass('nav-sm').addClass('nav-md');
 
@@ -51,7 +51,7 @@ $(function() {
                 $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
             }
 
-            $LOGO.children('img').attr('src',url+'assets/images/widecms_sm.png');
+            $LOGO.children('img').attr('src', url + 'assets/images/widecms_sm.png');
         }
 
         setContentHeight();
@@ -62,16 +62,39 @@ $(function() {
 
     $SIDEBAR_MENU.find('a').filter(function () {
         return this.href == URL;
-    }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
+    }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
         setContentHeight();
     }).parent().addClass('active');
+
+    $SIDEBAR_MENU.find('a').on('click', function (ev) {
+        var $li = $(this).parent();
+        
+        if ($li.is('.active')) {
+            $li.removeClass('active active-sm');
+            $('ul:first', $li).slideUp(function () {
+                setContentHeight();
+            });
+        } else {
+            // prevent closing menu if we are on child menu
+            if (!$li.parent().is('.child_menu')) {
+                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                $SIDEBAR_MENU.find('li ul').slideUp();
+            }
+
+            $li.addClass('active');
+
+            $('ul:first', $li).slideDown(function () {
+                setContentHeight();
+            });
+        }
+    });
 
 });
 // /Sidebar
 
 
 // Switchery
-$(document).ready(function() {
+$(document).ready(function () {
     if ($(".js-switch")[0]) {
         var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
         elems.forEach(function (html) {
