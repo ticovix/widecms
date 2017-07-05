d2a15e8661ee35b2f7c0ab5af3b91c054701a240
Patrick <contato@widedevelop.com>
2017-07-04 21:42:23 -0300

    Melhoria na estrutura

--- wd-admin/application/models/Permissions_model.php
+++ wd-admin/application/models/Permissions_model.php
@@ -9,19 +9,21 @@ class Permissions_model extends CI_Model
 
     public function check_app($app, $user_id)
     {
+        $allow = false;
         $this->db->select('status');
         $perm = $this->db->get_where('wd_users_perm', array('app' => $app, 'fk_user' => $user_id, 'page' => '', 'method' => ''))->row();
-        if ($perm) {
-            return $perm->status;
-        } else {
-            return false;
+        if (!empty($perm)) {
+            $allow = $perm->status;
         }
+
+        return $allow;
     }
 
     public function list_pages($app)
     {
         $this->db->select('status, page');
         $user_id = $this->user_data['id'];
+
         return $this->db->get_where('wd_users_perm', array('app' => $app, 'fk_user' => $user_id, 'page!=' => ''))->result_array();
     }
 
